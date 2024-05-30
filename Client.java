35c8e44c710f7f060443853f10825d65617e9eb1
TimWarfel20 <127687304+TimWarfel20@users.noreply.github.com>
2024-05-29 23:58:45 -0400

    Add files via upload

--- Client.java
+++ Client.java
@@ -9,25 +9,62 @@ import java.io.*;
 
 public class Client{
 
+public static String username = "";
 public static final int port = 4444;
 public static final String ip = "127.0.0.1";
 public static int numRows = 0;
 public static JFrame frame = new JFrame("Chat Application");
 public static JTextField textField = new JTextField();
 public static JButton button = new JButton("Input Message Here");
+public static JTextArea textArea = new JTextArea(10, 30);
 
 public static PrintWriter out;
 public static BufferedReader in;
 
-    public static void main(String args[]){
+    public static void main(String args[]) throws IOException{
         
     connectToServer();
-    displayWindow();
+    nameWindow();
+
+    while(true){
+        numRows++;
+        if(numRows >=10){
+            textArea.setText("");
+            numRows = 0;
+        }
+        String buffer = in.readLine(); 
+        textArea.setText(textArea.getText() + "\n" + buffer);
+    }
+    }
+
+    public static void nameWindow(){
+        JFrame nameFrame = new JFrame("Chat Application");
+        nameFrame.setSize(500, 500);
+
+        JButton nameButton = new JButton("Please Enter your Name");
+        nameButton.setBounds(100, 224, 300, 30);
+        nameFrame.add(nameButton);
+
+        JTextField nameField = new JTextField();
+        nameField.setBounds(100, 175, 300, 50);
+        nameFrame.add(nameField);
+
+        nameFrame.setLayout(null);
+        nameFrame.setVisible(true);
+        nameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        nameButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                username = nameField.getText();
+                displayWindow();
+                nameFrame.dispose();
+            }
+        });
     }
 
     public static void displayWindow(){
 
-        JTextArea textArea = new JTextArea(10, 30);
+       
         textArea.setEditable(false);
 
         frame.setSize(500,500);
@@ -40,30 +77,10 @@ public static BufferedReader in;
 
         button.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
-                numRows++;
-                if(numRows >=10){
-                    textArea.setText("");
-                    numRows = 0;
-                }
+              
                 String message = textField.getText();
+                message = username + ": " + message;
                 sendMessage(message);
-                try{
-                textArea.setText(textArea.getText() + "\n" + message);
-                new Thread(()->{
-                    try {
-                        while(true){
-                        System.out.println(in.readLine());
-                        }
-                    } catch (IOException AA) {
-                        // TODO Auto-generated catch block
-                        AA.printStackTrace();
-                    }
-                 
-                    }).start();
-               
-                }catch(Exception a){
-                    System.out.println(a);
-                }
             }
         });
         
@@ -71,33 +88,20 @@ frame.add(textArea);
 textArea.setBounds(100, 100, 300, 200);
         
 
-        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(null);
         frame.setVisible(true);
     }
 
     public static void connectToServer(){
 try{
-
-
-
-
      Socket clientSocket = new Socket(ip, port);
       out = new PrintWriter(clientSocket.getOutputStream(), true);
-      in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-      sendMessage("Connected!");
-
-      
-      
+      in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));   
 }
 catch(Exception e){
     System.out.println("Error: Failed to Connect");
 }
-      
-   
-        
-    
-
     }
 
     public static void sendMessage(String msg){
--- Server.java
+++ Server.java
@@ -4,46 +4,60 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class Server {
-  
-    public static int index = 0;
+
+    static ArrayList<Socket> activeSockets = new ArrayList<Socket>();
 public static void main(String args[]){
     try{
     System.out.println("Server has begun running. Waiting for Connection...");
-
     ServerSocket serverSocket = new ServerSocket(4444);
-   
-  
     while(true){
         Socket clientSocket = serverSocket.accept();
     
     if(clientSocket.isConnected()){
+        System.out.println("Client Connected");
+        activeSockets.add(clientSocket);
+        for(int i = 0; i < activeSockets.size(); i++){
+            PrintWriter out = new PrintWriter(activeSockets.get(i).getOutputStream(), true);
+        out.println("Client Connected");
+        }
+
     new Thread(()->{
-        index++;
-       
         try{
         BufferedReader in = new BufferedReader (new InputStreamReader(clientSocket.getInputStream()));
-        PrintWriter out = new PrintWriter(clientSocket.getOutputStream());
 
         while(true){
             String message = in.readLine();
             System.out.println(message);
-        out.println(message);
+
+            for(int i = 0; i < activeSockets.size(); i++){
+                PrintWriter out = new PrintWriter(activeSockets.get(i).getOutputStream(), true);
+            out.println(message);
+            }
         }
         }
         catch(Exception e){
-            index--;
             System.out.println("Client Disconnected");
+            for(int i = 0; i < activeSockets.size(); i++){
+                PrintWriter out;
+                try {
+                    out = new PrintWriter(activeSockets.get(i).getOutputStream(), true);
+                    out.println("Client Disconnected");
+                } catch (IOException e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                }
+            
+            }
         }
 
     }).start();
     }
     }
 }
-    catch(Exception e){
-        System.out.println("The client has disconnected. The server will now shut down");
-    }
+    catch(Exception e){}
 
 
 }
